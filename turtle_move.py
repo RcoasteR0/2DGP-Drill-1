6fdf87fe103101071ccba8b06bcdadf402f93b5a
RcoasteR0 <92726320+RcoasteR0@users.noreply.github.com>
2024-09-21 14:24:10 +0900

    Drill 1 제출 (2022180007 김용채)

---
+++ turtle_move.py
@@ -0,0 +1,34 @@
+import turtle
+
+def move_w():
+    turtle.stamp()
+    turtle.setheading(90)
+    turtle.forward(50)
+
+def move_a():
+    turtle.stamp()
+    turtle.setheading(180)
+    turtle.forward(50)
+
+def move_s():
+    turtle.stamp()
+    turtle.setheading(270)
+    turtle.forward(50)
+
+def move_d():
+    turtle.stamp()
+    turtle.setheading(0)
+    turtle.forward(50)
+
+def restart():
+    turtle.reset()
+
+turtle.shape('turtle')
+
+turtle.onkey(move_w, 'w')
+turtle.onkey(move_a, 'a')
+turtle.onkey(move_s, 's')
+turtle.onkey(move_d, 'd')
+turtle.onkey(restart, 'Escape')
+
+turtle.listen()
